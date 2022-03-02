2b20b34a5a0873aeba58f223efdc388eb7b33a31
camillapalombi <camilla.palombi14@gmail.com>
2022-03-02 18:41:43 +0100

    JS Prova

--- js/script.js
+++ js/script.js
@@ -41,22 +41,54 @@ for (let i = 1; i <= 49; i++) {
     squaresContainer.append(elemento);
 }*/
 
-let optionEasy = document.getElementById('easy').value;
-console.log(optionEasy);
-let optionDifficult = document.getElementById('difficult').value;
-console.log(optionDifficult);
-let optionMedium = document.getElementById('medium').value;
-console.log(optionMedium);
-
-let btnPlay = document.getElementById('btn-play').value;
-console.log(btnPlay);
-
-
-let select = document.getElementById('difficulty');
-let value = select.options[select.selectedIndex].value;
-console.log(value);
-
+let btnPlay = document.getElementById('btn-play');
+    console.log(btnPlay);
 
 btnPlay.addEventListener('click' , function() {
+    
+    
+    let optionEasy = document.getElementById('easy').value;
+    console.log(optionEasy);
+    let optionDifficult = document.getElementById('difficult').value;
+    console.log(optionDifficult);
+    let optionMedium = document.getElementById('medium').value;
+    console.log(optionMedium);
 
+    let valueSelect = document.getElementById('difficulty').value;
+    console.log(valueSelect)
+
+    let squaresContainer = document.querySelector('.container-play-grid');
+
+
+ if (valueSelect == optionEasy) {
+    
+    for (let i = 1; i <= 100; i++) {
+        
+        let elemento = document.createElement('div');
+        elemento.classList.add('square-100');
+        elemento.innerHTML = i ;
+        squaresContainer.append(elemento);
+    }
+ } else if (valueSelect == optionDifficult) {
+
+    for (let i = 1; i <= 49; i++) {
+        
+        let elemento = document.createElement('div');
+        elemento.classList.add('square-49');
+        elemento.innerHTML = i ;
+        squaresContainer.append(elemento);
+        
+    } 
+ } else {
+
+    for (let i = 1; i <= 81; i++) {
+        
+        let elemento = document.createElement('div');
+        elemento.classList.add('square-81');
+        elemento.innerHTML = i ;
+        squaresContainer.append(elemento);
+        
+    }
+ }
+ 
 })
