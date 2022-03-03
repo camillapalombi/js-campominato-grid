b311431ba266b1cfb69d2f5b2b3331929624ae6f
camillapalombi <camilla.palombi14@gmail.com>
2022-03-03 15:37:56 +0100

    JS Corretto

--- js/script.js
+++ js/script.js
@@ -1,94 +1,53 @@
-/*DIFFICOLTà FACILE
+let valueSelect = document.getElementById('difficulty');
 
 let squaresContainer = document.querySelector('.container-play-grid');
-
-
-for (let i = 1; i <= 100; i++) {
-
-    let elemento = document.createElement('div');
-    elemento.classList.add('square-100');
-    elemento.innerHTML = i ;
-
-    squaresContainer.append(elemento);
-}*/
-
-/*DIFFICOLTà MEDIA
-
-let squaresContainer = document.querySelector('.container-play-grid');
-
-
-for (let i = 1; i <= 81; i++) {
-
-    let elemento = document.createElement('div');
-    elemento.classList.add('square-81');
-    elemento.innerHTML = i ;
-
-    squaresContainer.append(elemento);
-}*/
-
-
-/*DIFFICOLTà DIFFICILE
-
-let squaresContainer = document.querySelector('.container-play-grid');
-
-
-for (let i = 1; i <= 49; i++) {
-
-    let elemento = document.createElement('div');
-    elemento.classList.add('square-49');
-    elemento.innerHTML = i ;
-
-    squaresContainer.append(elemento);
-}*/
 
 let btnPlay = document.getElementById('btn-play');
-    console.log(btnPlay);
-
-btnPlay.addEventListener('click' , function() {
-    
-    
-    let optionEasy = document.getElementById('easy').value;
-    console.log(optionEasy);
-    let optionDifficult = document.getElementById('difficult').value;
-    console.log(optionDifficult);
-    let optionMedium = document.getElementById('medium').value;
-    console.log(optionMedium);
-
-    let valueSelect = document.getElementById('difficulty').value;
-    console.log(valueSelect)
-
-    let squaresContainer = document.querySelector('.container-play-grid');
 
 
- if (valueSelect == optionEasy) {
-    
-    for (let i = 1; i <= 100; i++) {
+/*ADD EVENT LISTENER*/
+
+btnPlay.addEventListener('click' , play);
+
+arrLevel = [100, 81, 49];
+
+function play() {
+
+    squaresContainer.innerHTML = '';
+
+    let IndexLevel = parseInt(valueSelect.value);
+
+    let cellsCount = arrLevel[IndexLevel];
+
+    if (cellsCount == 100) {
+
+        for (let i = 1; i <= 100; i++) {
+
+            let elemento = document.createElement('div');
+            elemento.classList.add('square-100');
+            elemento.innerHTML = i ;
+            squaresContainer.append(elemento);
+        }
+
+    } else if (cellsCount == 49) {
+
+        for (let i = 1; i <= 49; i++) {
         
-        let elemento = document.createElement('div');
-        elemento.classList.add('square-100');
-        elemento.innerHTML = i ;
-        squaresContainer.append(elemento);
-    }
- } else if (valueSelect == optionDifficult) {
+            elemento = document.createElement('div');
+            elemento.classList.add('square-49');
+            elemento.innerHTML = i ;
+            squaresContainer.append(elemento);
+        
+        } 
 
-    for (let i = 1; i <= 49; i++) {
+    } else {
+
+        for (let i = 1; i <= 81; i++) {
         
-        let elemento = document.createElement('div');
-        elemento.classList.add('square-49');
-        elemento.innerHTML = i ;
-        squaresContainer.append(elemento);
-        
+            elemento = document.createElement('div');
+            elemento.classList.add('square-81');
+            elemento.innerHTML = i ;
+            squaresContainer.append(elemento);  
+        }
     } 
- } else {
-
-    for (let i = 1; i <= 81; i++) {
-        
-        let elemento = document.createElement('div');
-        elemento.classList.add('square-81');
-        elemento.innerHTML = i ;
-        squaresContainer.append(elemento);
-        
-    }
- }
- 
-})
+}
